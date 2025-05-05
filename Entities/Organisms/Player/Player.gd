166fa9a98d81fdb69f53cb99c88ad7f0ff282fef
Đậu Khoa <khoakomlem@gmail.com>
2025-05-06 00:26:31 +0700

    use item and deal damage!

---
+++ Entities/Organisms/Player/Player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var stats_component: StatsComponent = $StatsComponent
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var item_manager: ItemManager = $ItemManager
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+
+func _physics_process(_delta: float) -> void:
+	item_manager.scale.y = sign(cos(rotation))
+	move_and_slide()
+
+func _process(_delta: float) -> void:
+	item_manager.scale.y = sign(cos(rotation))
+
+
+func _on_hurtbox_received_damage(damage: int) -> void:
+	print("Received damage: ", damage, owner)
+	animation_player.play("hit")
+	pass # Replace with function body.
