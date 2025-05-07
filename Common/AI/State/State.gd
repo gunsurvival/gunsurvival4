2951219a32d59356b328358baf24c65c215ab2c6
Đậu Khoa <khoakomlem@gmail.com>
2025-05-07 23:39:25 +0700

    chore

--- Common/AI/State/State.gd
+++ Common/AI/State/State.gd
@@ -11,11 +11,10 @@ var mob_movement: MobMovement
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(owner)
 	await owner.ready
-	mob_movement = owner as MobMovement
-	# print(mob_movement, self.name)
-	# assert(mob_movement != null, "ModMovementState must be attached to a MobMovement node.")
+	if owner is MobMovement:
+		mob_movement = owner as MobMovement
+	assert(mob_movement != null, "ModMovementState must be attached to a MobMovement node.")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
