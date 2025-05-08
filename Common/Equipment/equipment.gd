b11a8db4ac09749550c6df910d39ad9522a47e99
Đậu Khoa <khoakomlem@gmail.com>
2025-05-08 20:06:55 +0700

    fix hand update

--- Common/Equipment/equipment.gd
+++ Common/Equipment/equipment.gd
@@ -1,12 +1,10 @@
 extends Node2D
 class_name Equipment
 
-
 @export var stats: StatsComponent
-@export var head: Item : set = set_head
-@export var body: Item : set = set_body
-@export var lefthand: Item: set = set_lefthand
-@export var righthand: Item: set = set_righthand
+@export var head: Item: set = set_head
+@export var body: Item: set = set_body
+@export var hand: Item: set = set_hand
 @export var accessories: Array[Item] = []
 
 
@@ -16,45 +14,53 @@ func _ready() -> void:
 
 
 func hand_trigger() -> void:
-	if righthand != null:
-		righthand.trigger()
-
-	if lefthand != null:
-		lefthand.trigger()
+	if hand != null:
+		hand.trigger()
 
 
 func hand_stop_trigger() -> void:
-	if righthand != null:
-		righthand.stop_trigger()
+	if hand != null:
+		hand.stop_trigger()
 
-	if lefthand != null:
-		lefthand.stop_trigger()
 
 func set_head(item: Item) -> void:
 	head = item
 	head.emit_signal("holded")
 
+
 func set_body(item: Item) -> void:
 	body = item
 	body.emit_signal("holded")
 
-func set_lefthand(item: Item) -> void:
-	lefthand = item
-	item.visible = true
-	item.rotation = +PI/16
-	item.position.x = 70
-	item.position.y = -20
-	lefthand.emit_signal("holded")
 
-func set_righthand(item: Item) -> void:
-	righthand = item
-	item.visible = true
-	item.rotation = -PI/16
-	item.position.x = 70
-	item.position.y = 20
-	righthand.emit_signal("holded")
+func set_hand(item: Item) -> void:
+	_switch_from_to(hand, item)
+	hand = item
+
 
 func add_accessory(item: Item) -> void:
 	item.equipment = self
 	item.visible = false
 	accessories.append(item)
+
+
+func _switch_from_to(current: Item, to: Item) -> void:
+	if current != null:
+		current.visible = false
+		current.is_triggering = false
+		current.stop_trigger()
+
+	if to != null:	
+		to.visible = true
+		to.rotation = -PI/16
+		to.position.x = 70
+		to.position.y = 20
+		to.emit_signal("holded")
+
+
+func switch_to_index(index: int) -> void:
+	if index < 0 or index >= accessories.size():
+		hand = null
+		return
+
+	hand = accessories[index]
