d19413e793a7ab375fd8965fa94adfffa67bac48
Đậu Khoa <khoakomlem@gmail.com>
2025-05-08 20:31:44 +0700

    feat: item slot
    close #1

--- UI/HUD/HUD.gd
+++ UI/HUD/HUD.gd
@@ -1,4 +1,5 @@
 extends CanvasLayer
+class_name HUD
 
 var ItemSlotScene: PackedScene = preload("res://UI/HUD/ItemSlot/ItemSlot.tscn")
 
@@ -6,13 +7,15 @@ var ItemSlotScene: PackedScene = preload("res://UI/HUD/ItemSlot/ItemSlot.tscn")
 @export var player: Player
 @onready var hbox_container: HBoxContainer = $HBoxContainer
 
-
 func _ready() -> void:
+	player.equipment.connect("slot_changed", _on_slot_changed)
+
 	for i in range(max_slots):
 		var child = hbox_container.get_child(i)
 
 		if child == null:
 			child = ItemSlotScene.instantiate()
+			child.owner = self
 			hbox_container.add_child(child)
 
 		if child is ItemSlot and i < player.equipment.accessories.size():
@@ -20,17 +23,62 @@ func _ready() -> void:
 
 	# Always set the right hand to the first accessory (if any)
 	if player.equipment.accessories.size() > 0:
-		player.equipment.righthand = player.equipment.accessories[0]
-		player.equipment.lefthand = player.equipment.accessories[1]
+		player.equipment.current_slot = 0
+		# player.equipment.lefthand = player.equipment.accessories[1]
 
 
 func resize_texture_to_fit(tex: Texture2D, boundary: Vector2i) -> Texture2D:
 	var original_size := tex.get_size()
 	var new_scale := min(boundary.x / original_size.x, boundary.y / original_size.y) as float
-	var new_size := Vector2i(original_size.x * new_scale, original_size.y * new_scale)
+	var new_size := Vector2(original_size.x * new_scale, original_size.y * new_scale)
 
 	var img := tex.get_image()
 	img.resize(new_size.x, new_size.y, Image.INTERPOLATE_TRILINEAR)
 
 	var new_tex := ImageTexture.create_from_image(img)
 	return new_tex
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("slot1"):
+			choose_slot(0)
+		if event.is_action_pressed("slot2"):
+			choose_slot(1)
+		if event.is_action_pressed("slot3"):
+			choose_slot(2)
+		if event.is_action_pressed("slot4"):
+			choose_slot(3)
+		if event.is_action_pressed("slot5"):
+			choose_slot(4)
+		if event.is_action_pressed("slot6"):
+			choose_slot(5)
+		if event.is_action_pressed("slot7"):
+			choose_slot(6)
+		if event.is_action_pressed("slot8"):
+			choose_slot(7)
+		if event.is_action_pressed("slot9"):
+			choose_slot(8)
+
+func choose_slot(index: int) -> void:	
+	player.equipment.current_slot = index
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		var slot_count = hbox_container.get_child_count()
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			player.equipment.current_slot = (player.equipment.current_slot - 1 + slot_count) % slot_count
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			player.equipment.current_slot = (player.equipment.current_slot + 1) % slot_count
+
+
+func _on_slot_changed() -> void:
+	for i in range(hbox_container.get_child_count()):
+		var slot = hbox_container.get_child(i)
+		if slot is ItemSlot:
+			if i == player.equipment.current_slot:
+				slot.texture_rect.modulate = Color(1, 1, 1, 1)  # Fully visible
+				slot.thumbnail.modulate = Color(1, 1, 1, 1)
+			else:
+				slot.texture_rect.modulate = Color(1, 1, 1, 0.8)  # Semi-transparent
+				slot.thumbnail.modulate = Color(1, 1, 1, 0.8)
--- UI/HUD/ItemSlot/ItemSlot.gd
+++ UI/HUD/ItemSlot/ItemSlot.gd
@@ -3,3 +3,15 @@ class_name ItemSlot
 
 @onready var thumbnail: TextureRect = $CenterContainer/Thumbnail
 @onready var texture_rect: TextureRect = $TextureRect
+
+var hud: HUD
+
+func _ready() -> void:
+	# await owner.ready
+	if owner is HUD:
+		hud = owner as HUD
+	# assert(hud, "ItemSlot must be a child of HUD")
+
+func _on_pressed() -> void:
+	var index = get_index()
+	hud.choose_slot(index)
