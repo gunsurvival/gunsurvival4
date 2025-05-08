81ec4b26f5a260430dfb0e910e422a3cb61f4743
Đậu Khoa <khoakomlem@gmail.com>
2025-05-08 20:07:55 +0700

    add normal random to bullet projectile

--- Entities/Items/Tools/AK47/AK47.gd
+++ Entities/Items/Tools/AK47/AK47.gd
@@ -6,8 +6,22 @@ var Bullet: PackedScene = preload("res://Entities/Items/Other/Bullet/Bullet.tscn
 
 func _on_used() -> void:
 	var bullet = Bullet.instantiate()
-	bullet.position = owner.position + Vector2(cos(owner.rotation), sin(owner.rotation)) * 60
-	bullet.rotation = owner.rotation
+	
+	var spread_degrees = 10.0  # total spread range
+	var std_dev = spread_degrees / 3.0  # ~99% within ±spread
+
+	var spread_radians = deg_to_rad(rand_normal(0.0, std_dev))
+	var rot =  owner.rotation + spread_radians
+
+	bullet.position = owner.position + Vector2(cos(rot), sin(rot)) * 60
+	bullet.rotation = rot
+	bullet.linear_velocity  = Vector2(1, 0).rotated(rot) * 2000
 	bullets.add_child(bullet)
 
 	pass # Replace with function body.
+
+func rand_normal(mean: float = 0.0, stddev: float = 1.0) -> float:
+	var u1 = randf()
+	var u2 = randf()
+	var z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2)
+	return z0 * stddev + mean
