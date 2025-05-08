631666778551f3ba3519bef205f61ce364bc74e1
Đậu Khoa <khoakomlem@gmail.com>
2025-05-08 20:07:39 +0700

    add linear_damp, fix rotation to sprite

--- Entities/Items/Other/Bullet/Bullet.gd
+++ Entities/Items/Other/Bullet/Bullet.gd
@@ -1,14 +1,25 @@
 extends RigidBody2D
 
 @onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
 	# Set the initial velocity of the bullet
 	# velocity_component.velocity = 
-	linear_velocity  = Vector2(1, 0).rotated(rotation) * 1000
+	pass
 
 
 func _on_hitbox_hit() -> void:
 	queue_free()
 	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	if linear_velocity.length() < 10:
+		queue_free()
+		return
+
+func _process(delta: float) -> void:
+	sprite_2d.rotation = -rotation  + atan2(linear_velocity.y, linear_velocity.x)
+	# print(rotation)
